71c8c6af857a383b68e84491ce5b9eb7ca08f81e
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2022-09-18 14:43:10 -0600

    post endpoint to set the data to dgraph

--- endpoint.go
+++ endpoint.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/dgraph-io/dgo/v210/protos/api"
 )
 
 const getUser = `
@@ -28,4 +31,63 @@ func getAllPrograms(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 	w.Write(resp.Json)
+}
+
+var decode struct {
+	All []struct {
+		id   string
+		name string
+		age  string
+	}
+}
+
+// const setNodes Nodes = `
+// {
+// 	"set":[
+// 	  {
+// 		"name": "Jojs",
+//       	"age": 42,
+// 	  }
+// 	]
+//   }
+// `
+
+func setAllPrograms(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+	dgClient := newClient()
+	txn := dgClient.NewTxn()
+
+	// input := &model{
+	// 	r.Body
+	// }
+	fmt.Print(r.Body)
+	inputJson, err := json.Marshal(r.Body)
+
+	mu := &api.Mutation{
+		SetJson:   inputJson,
+		CommitNow: true,
+	}
+	log.Println("mutation object:", mu)
+
+	// defer txn.Discard(ctx)
+	resp, err := txn.Mutate(context.Background(), mu)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write(resp.Json)
+
+	// txn := dgClient.NewTxn()
+
+	// resp, err := txn.Query(context.Background(), setNodes)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// out, err := json.Marshal(decode.All)
+	// if err != nil {
+	//     log.Fatal(err)
+	// }
+
+	// _, err := txn.Mutate(context.Background(), &api.Mutation{SetJson: out})
 }
--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"time"
 
+	"github.com/dgraph-io/dgo/v210/protos/api"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
@@ -37,6 +39,49 @@ func main() {
 		txn := dgClient.NewTxn()
 
 		resp, err := txn.Query(context.Background(), getUser)
+		if err != nil {
+			log.Fatal(err)
+		}
+		w.Write(resp.Json)
+	})
+
+	type Data struct {
+		number string
+		result int
+		total  int
+		num1   string
+		num2   string
+		option string
+		assign string
+	}
+
+	type Program struct {
+		class    string
+		html     string
+		data     Data
+		id       int
+		pos_x    float64
+		pos_y    float64
+		typenode string
+	}
+
+	router.Post("/setAllPrograms", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		var p Program
+		newjson := json.NewDecoder(r.Body).Decode(&p)
+		dgClient := newClient()
+		txn := dgClient.NewTxn()
+
+		fmt.Printf("body", r.Body)
+		inputJson, err := json.Marshal(newjson)
+
+		mu := &api.Mutation{
+			SetJson:   inputJson,
+			CommitNow: true,
+		}
+		log.Println("mutation object:", mu)
+
+		resp, err := txn.Mutate(context.Background(), mu)
 		if err != nil {
 			log.Fatal(err)
 		}
