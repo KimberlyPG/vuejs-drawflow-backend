dae8629eac50432afc0c0292b74e1031d3631b1c
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2022-09-20 21:52:54 -0600

    change in the struct and using docker insted of dgraph cloud

--- dgraph.go
+++ dgraph.go
@@ -2,29 +2,29 @@ package main
 
 import (
 	"log"
-	"os"
 
 	"github.com/dgraph-io/dgo/v210"
 	"github.com/dgraph-io/dgo/v210/protos/api"
-	"github.com/joho/godotenv"
+	"google.golang.org/grpc"
 )
 
-func goDotEnvVariable(key string) string {
+// func goDotEnvVariable(key string) string {
 
-	// load .env file
-	err := godotenv.Load(".env")
+// 	// load .env file
+// 	err := godotenv.Load(".env")
 
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
+// 	if err != nil {
+// 		log.Fatalf("Error loading .env file")
+// 	}
 
-	return os.Getenv(key)
-}
+// 	return os.Getenv(key)
+// }
 
 func newClient() *dgo.Dgraph {
-	dotenv := goDotEnvVariable("key")
+	// dotenv := goDotEnvVariable("key")
 	// fmt.Printf("godotenv : %s = %s \n", "KEY", dotenv)
-	conn, err := dgo.DialCloud("https://nameless-brook-350164.eu-central-1.aws.cloud.dgraph.io/graphql", dotenv)
+	// conn, err := dgo.DialCloud("https://nameless-brook-350164.eu-central-1.aws.cloud.dgraph.io/graphql", dotenv)
+	conn, err := grpc.Dial("localhost:9080", grpc.WithInsecure())
 	if err != nil {
 		log.Fatal(err)
 	}
--- endpoint.go
+++ endpoint.go
@@ -2,90 +2,55 @@ package main
 
 const getNodeData = `
 {
-  get(func: uid(0xfffd8d6ab2de70ec)) {
-      data {
-        id
-        name
-        data{
-          number
-          result
-        }
-        class
-        html
-        typenode
-        inputs{
-          input_1{
-              connections{
+    get(func: has(nodesData)) {
+        uid
+        nodesData (orderasc: id){
+            id
+            name
+            data {
+                number
+                result
+                variable
+                assign
+                num1
+                num2
+                conditionResult
+                option
+            }
+            class
+            html
+            typenode
+            inputs{
+            input_1{
+                connections{
+                    node
+                    input
+                }
+            }
+            input_2{
+                connections{
+                    node
+                    input
+                }
+            }
+            }
+            outputs{
+            output_1{
+                connections{
+                    node
+                    output
+                }
+            }
+            output_2{
+                connections{
                 node
                 input
+                }
             }
-          }
-          input_2{
-            connections{
-                node
-                input
             }
-          }
+            pos_x
+            pos_y
         }
-        outputs{
-          output_1{
-            connections{
-                  node
-                  output
-            }
-          }
-          output_2{
-            connections{
-              node
-              input
-            }
-          }
-        }
-        pos_x
-        pos_y
-     }
-   }
-  }
+    }
+}
 `
-
-// func getAllPrograms(w http.ResponseWriter, r *http.Request) {
-// 	w.Header().Set("Content-Type", "application/json")
-// 	dgClient := newClient()
-// 	fmt.Print(dgClient)
-// 	txn := dgClient.NewTxn()
-
-// 	resp, err := txn.Query(context.Background(), getUser)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	w.Write(resp.Json)
-// }
-
-// type Connections struct {
-// 	Node string `json:"node"`
-// 	Uid  string `json:"uid"`
-// }
-
-// func setAllPrograms(w http.ResponseWriter, r *http.Request) {
-
-// 	w.Header().Set("Content-Type", "application/json")
-// 	var p Connections
-// 	newjson := json.NewDecoder(r.Body).Decode(&p)
-// 	dgClient := newClient()
-// 	txn := dgClient.NewTxn()
-
-// 	fmt.Printf("body", r.Body)
-// 	inputJson, err := json.Marshal(newjson)
-
-// 	mu := &api.Mutation{
-// 		SetJson:   inputJson,
-// 		CommitNow: true,
-// 	}
-// 	log.Println("mutation object:", mu)
-
-// 	resp, err := txn.Mutate(context.Background(), mu)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	w.Write(resp.Json)
-// }
